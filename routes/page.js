7f6876b37dfcda88326f057657fcf766b0aafa46
app-demo <1059870337@qq.com>
2018-05-30 17:50:44 +0800

    Undersigned: app-demo <1059870337@qq.com>

--- routes/page.js
+++ routes/page.js
@@ -4,13 +4,15 @@ var page = require("../DAO/page");
 router.get('/gamePage', function (req, res, next) {
     var pageCount = 0;//总页数
     var sizeCount = 0;//总条数，总行数
-    var p_num = 10;//每页显示数量，输出大小，暂定10条
+    var p_num = 15;//每页显示数量，输出大小，暂定10条
     var lastPage = "";//是否最后一页
     var firstPage = "";//是否第一页
     var arr = {};//最后返回
     var p = 1;//当前页数,默认第一页
-    var nextPage = 2;//下一页
-    var prevPage = 1;//上一页
+    var nextPage = 2;//下一页，默认第二页
+    var prevPage = 1;//上一页，默认第一页
+    var page_num = [];//数组默认显示10页
+    var pa = parseFloat(req.query.pa);//页数分隔标记
 
     if (parseFloat(req.query.p) > 0) {
         p = parseFloat(req.query.p);//当前页数
@@ -21,14 +23,33 @@ router.get('/gamePage', function (req, res, next) {
             firstPage = 1
         }
     } else {
-        firstPage = 1
+        firstPage = 1//获得p小于1时
+    }
+
+    if (p > 10) {
+        if ((10 * pa + 1) / p == 1) {//11，21，31页时改变页码数
+            for (var i = p; p <= i <= 10 * (pa + 1); i++) {
+                if (i > 10 * (pa + 1)) {
+                    break;
+                }
+                console.log(i);
+                page_num.push(i)
+            }
+        }
+    } else {
+        for (var i = 1; i <= 10; i++) {
+            if (i > 10) {
+                break;
+            }
+            page_num.push(i)
+        }
     }
 
     page.getGamePageCount(function (resultCount) {
         sizeCount = resultCount.length;//总记录数
         // res.json({pageCount: pageCount});
         if (sizeCount > 0) {
-            pageCount = sizeCount % p_num == 0 ? sizeCount / p_num : sizeCount / p_num + 1;//总页数
+            pageCount = sizeCount % p_num == 0 ? sizeCount / p_num : sizeCount / p_num + 1;//计算总页数
 
             pageCount = parseInt(pageCount);//去除小数点
             if (pageCount < p) {//当传递的页数大于最大的页数时固定在最后一页
@@ -48,9 +69,10 @@ router.get('/gamePage', function (req, res, next) {
                     lastPage: lastPage,//判断是否为尾页
                     nextPage: nextPage,//下一页
                     prevPage: prevPage,//上一页
-                    totalPage: pageCount//总页数
+                    totalPage: pageCount,//总页数
+                    page_num: page_num//输出页数
                 };
-                res.json(arr)
+                res.json(arr)//返回数据
             });
         }
     })
